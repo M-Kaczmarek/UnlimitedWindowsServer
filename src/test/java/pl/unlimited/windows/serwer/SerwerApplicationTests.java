549cd077ff066229a61f6e5f94f036c7ce813639
Mariusz Kaczmarek <mariuszkaczmarek@MacBook-Air-Mariusz.local>
2021-07-19 23:14:42 +0200

    Init Spring boot application

---
+++ src/test/java/pl/unlimited/windows/serwer/SerwerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.unlimited.windows.serwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
