510c61973a0563dae4b1bae409c41d2cb8384f7f
M-Kaczmarek <mariuszkaczmarek97@outlook.com>
2021-09-07 15:06:01 +0200

    added new exception to rest advicer

--- src/main/java/pl/unlimited/windows/serwer/error/RestErrorAdvice.java
+++ src/main/java/pl/unlimited/windows/serwer/error/RestErrorAdvice.java
@@ -6,20 +6,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import pl.unlimited.windows.serwer.error.exception.ErrorCode;
-import pl.unlimited.windows.serwer.error.exception.OrderDocumentNotFoundException;
-import pl.unlimited.windows.serwer.error.exception.ServiceException;
+import pl.unlimited.windows.serwer.error.exception.*;
 
 @ControllerAdvice
 public class RestErrorAdvice extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(value = {})
+    @ExceptionHandler(value = {OrderDocumentInvalidRequestException.class})
     public ResponseEntity<Error> handleBadRequest(ServiceException e) {
         Error error = new Error(e.getMessage(), e.getErrorCode());
 
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(value = {OrderDocumentNotFoundException.class})
+    @ExceptionHandler(value = {OrderDocumentNotFoundException.class, WindowSizeNotFoundException.class})
     public ResponseEntity<Error> handleNotFound(ServiceException e) {
         Error error = new Error(e.getMessage(), e.getErrorCode());
 
